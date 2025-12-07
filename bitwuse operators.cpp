69808a91fc62ec4dad439834ec9713b32872a936
kuruvasandhya838-sandy <kuruvasandhya838@gmail.com>
2025-12-07 17:02:57 +0530

    Add files via upload

---
+++ bitwuse operators.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ int main()
+{
+int a = 25, b = 5;
+printf("a & b: %d\n", a & b);
+printf("a | b: %d\n", a | b);
+printf("a ^ b: %d\n", a ^ b);
+printf("~a: %d\n", ~a);
+printf("a >> b: %d\n", a >> b);
+ printf("a << b: %d\n", a << b);
+  return 0;
+}
+
