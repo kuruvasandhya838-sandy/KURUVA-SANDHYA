c4a9d3189a6fdcedab9f4454a99799a0e869e6d1
kuruvasandhya838-sandy <kuruvasandhya838@gmail.com>
2025-12-07 16:58:12 +0530

    Add files via upload

---
+++ 2D ARRAY.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>  
+   main()
+{      
+int i=0,j=0;    
+int arr[3][3]={{1,2,3},{2,3,4},{3,4,5}};     
+//traversing 2D array    
+for(i=0;i<=2;i++)
+{    
+ for(j=0;j<=2;j++)
+{    
+   printf("arr[%d] [%d] = %d \n",i,j,arr[i][j]);    
+ }//end of j    
+}//end of i     
+}  
+
